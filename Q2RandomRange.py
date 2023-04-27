dd21b3f8ff5e5ab3dd7964c2a0538e1dd1d9fa2f
Airnov <124378651+Airnov@users.noreply.github.com>
2023-04-27 13:48:32 +0000

    comit

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,9 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+
+import random
+
+a = int(input("input a random whole number: "))
+b = int(input("input another random whole number: "))
+num1 = random.randint(a, b)
+print(num1) 
